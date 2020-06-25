28ad2a442cb710066c2bd0be6e56a8df120fceb2
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-25 23:45:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+  element_pos = 0
+  new_collection = []
+  while element_pos < collection.length 
+    yield(collection[element_pos])
+    new_collection << 
+    element_pos += 1 
+  end
+  
 
