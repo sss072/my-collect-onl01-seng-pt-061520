9cbdeef9140cc421415c6ff0373b6dbe9cb124c0
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-25 23:46:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,10 @@ def my_collect(collection)
   element_pos = 0
   new_collection = []
   while element_pos < collection.length 
-    yield(collection[element_pos])
-    new_collection << 
+    new_collection << yield(collection[element_pos])
     element_pos += 1 
   end
+  new_collection
+end
   
 
