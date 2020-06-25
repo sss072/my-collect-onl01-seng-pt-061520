d8278c39a62ea2b9f23e5be195ba1468ff4c976e
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-25 23:46:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(collection)
+  element_pos = 0
+  new_collection = []
+  while element_pos < collection.length 
+    new_collection << yield(collection[element_pos])
+    element_pos += 1 
+  end
+  new_collection
+end
+  
 
